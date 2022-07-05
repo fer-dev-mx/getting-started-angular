841ab9687a720253a174362432970b63117afa4c
Fer R <fer.devmx92@gmail.com>
2022-07-05 16:31:37 -0500

    Adding navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,23 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductAlertsComponent,
-    ProductListComponent,
-    TopBarComponent
-  ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent }
     ])
+  ],
+  declarations: [
+    AppComponent,
+    ProductAlertsComponent,
+    ProductListComponent,
+    TopBarComponent,
+    ProductDetailsComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
